4e68f05d2c9e139521fd941fb702ef2905731205
spy <alfred@stensatter.se>
2023-02-15 23:26:05 +0100

    init

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct piece{
+    int pos;
+    char t;
+};
+
+int pos; 
+
+int* getValidMoves(struct piece p){
+
+}
+
+int main(){
+    
+
+    return 0;
+}
+
