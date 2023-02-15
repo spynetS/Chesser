a6957f6fbbb434d4f820eaf24d3c9e33be50e149
spy <alfred@stensatter.se>
2023-02-16 00:00:30 +0100

    fixed fen

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include "render.h"
 
-/* char* fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; */
-char* fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+char* fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+/* char* fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"; */
 
 struct piece{
     int pos;
